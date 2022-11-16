36846ee9feffe8c9f22f7e3191d2a3df205d24fd
Umang Shrestha <umang@lifesignals.com>
2022-11-15 21:45:40 -0500

    updated dependencies

--- App.tsx
+++ App.tsx
@@ -1,34 +1,24 @@
+import React, { useState } from 'react';
+import Styles from './App.styles';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native';
-import { Login } from './components/Login';
-import Styles from './App.styles';
-import React, { useState } from 'react';
+import { Login } from './components/login';
 import store, { persistorStore as persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { LoadingMarkup } from './components/LoadingMarkup';
-import { Provider, useDispatch, useSelector } from 'react-redux';
-import { RootState } from './redux/types';
-
-const ChatApp = () => {
-  const name = useSelector((state: RootState) => state.user.name);
-  const dispatch = useDispatch();
-
-  return (<>
-    <Login />
-    <StatusBar style="auto" />
-  </>)
-}
-
+import { LoadingMarkup } from './components/loading-markup';
+import { Provider } from 'react-redux';
+import { Chat } from './components/chat/chat.component';
 
 export default function App() {
+  const [isChat, setIsChat] = useState(false);
+
   return (
     <Provider store={store}>
       <PersistGate loading={<LoadingMarkup />} persistor={persistor}>
         <SafeAreaView style={Styles.container}>
-          <ChatApp />
+          {!isChat ? <Login onPush={() => setIsChat(true)} /> : <Chat />}
         </SafeAreaView>
       </PersistGate>
     </Provider>
-
   );
 }
